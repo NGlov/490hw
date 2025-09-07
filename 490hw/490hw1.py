a68425b98d69981c8830108e5a0ea8c1e2543037
NGlov <nlg@njit.edu>
2025-09-07 18:08:49 -0400

    Add files via upload

---
+++ 490hw/490hw1.py
@@ -0,0 +1,22 @@
+import requests
+
+url = "https://student-info-api.netlify.app/.netlify/functions/submit_student_info"
+
+UCID = "nlg"
+SECTION = "101"
+
+studentinfo = {
+    "UCID": "nlg",
+    "first_name": "Nekhi",
+    "last_name": "Glover",
+    "github_username": "Nglov",
+    "discord_username": "ihkenn",
+    "favorite_cartoon": "Gravity Falls",
+    "favorite_language": "Python",
+    "movie_or_game_or_book": "Red dead redemption 2",
+    "section": "101"
+}
+
+response = requests.post(url, json=studentinfo)
+print("Status: ", {response.status_code})
+print(response.text)
